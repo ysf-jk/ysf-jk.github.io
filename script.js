a3413fde044ad71e660d720c563edac2fbd42503
ysy-255 <ysy-255@outlook.com>
2024-05-03 16:40:40 +0900

    結局position: absolute;をやめたのでいらなくなった

--- script.js
+++ script.js
@@ -51,10 +51,10 @@ function position_update(){
 	var height;
 	var copyright = document.getElementById("copyright");
 	if(copyright.style.position === "fixed"){
-		height = document.body.offsetHeight + 16/*一行の高さ*/ /*+ 100*/ + copyright.offsetHeight;
+		height = document.body.offsetHeight + 16/*一行の高さ*/ + copyright.offsetHeight;
 	}
 	else{
-		height = document.body.offsetHeight + 16 /*+ 100*/;
+		height = document.body.offsetHeight + 16;
 	}
 
 	// コピーライトの表示位置設定
